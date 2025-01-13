0ae3e0ee0bceb545b1cd9e9267d87b6d845e2e37
Valeri <v.markow.97@gmail.com>
2025-01-13 16:19:42 +0200

    Adding services for cars.

---
+++ CarManage.Server/Models/ServiceHistory.cs
@@ -0,0 +1,33 @@
+﻿using CarManage.Server.Models;
+
+namespace CarManage.Server.Models
+
+{
+    public class ServiceHistory
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int Services { get; set; } //Bitmask field for multiple services
+        public DateTime ServiceDate { get; set; }
+        public int OdometerAtService { get; set; }
+        public string Notes { get; set; }
+
+        public Car Car { get; set; }
+
+        public List<ServiceType> SelectedServices
+        {
+            get
+            {
+                var selectedServices = new List<ServiceType>();
+                foreach (ServiceType service in Enum.GetValues(typeof(ServiceType)))
+                {
+                    if ((Services & (int)service) != 0)
+                    {
+                        selectedServices.Add(service);
+                    }
+                }
+                return selectedServices;
+            }
+        }
+    }
+}
---
+++ CarManage.Server/Models/ServiceType.cs
@@ -0,0 +1,23 @@
+﻿namespace CarManage.Server.Models
+{
+    public enum ServiceType
+    {
+        OilChange = 1,
+        BrakePads = 2,
+        FilterChange = 4,
+        TireRotation = 8,
+        BatteryCheck = 16,
+        TransmissionFluidChange = 32,
+        EngineFlush = 64,
+        CoolantFlush = 128,
+        SparkPlugReplacement = 256,
+        TimingBeltReplacement = 512,
+        FuelInjectionCleaning = 1024,
+        Alignment = 2048,
+        SuspensionCheck = 4096,
+        ACRecharge = 8192,
+        DifferentialFluidChange = 16384,
+        TimingChainReplacement = 32768,
+        ClutchReplacement = 65536
+    }
+}
